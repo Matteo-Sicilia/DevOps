813f13d6dabd763e79d454ceea123242ffbab68a
Matteo Sicilia <matteo.sicilia@outlook.it>
2024-10-30 16:47:24 +0100

    Initialize HTTP server with basic "Hello World" response

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+// Create an HTTP server that responds with "Hello World"
+const server = http.createServer((req, res) => {
+    res.statusCode = 200; // Set status code to 200 (OK)
+    res.setHeader('Content-Type', 'text/plain'); // Set content type header to plain text
+    res.end('Git Test\n'); // Send response body "Hello World"
+});
+
+// Define port number and start listening for requests
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
